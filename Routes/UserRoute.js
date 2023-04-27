9c7e32d31be99adf97a0ef6ddbfd18101447468e
anuj2225 <anuj.sachdeva@appwrk.com>
2023-04-27 11:59:46 +0530

    first commit

---
+++ Routes/UserRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { deleteUser, followUser, getAllUser, getUser, unfollowUser, updateUser } from "../Controllers/UserCopntroller.js";
+import authMiddleWare from "../MiddleWare/authMiddeWare.js";
+const router = express.Router()
+
+router.get('/',getAllUser);
+router.get('/:id',getUser);
+router.put('/:id',authMiddleWare,updateUser);
+router.delete('/:id',authMiddleWare,deleteUser);
+router.put('/:id/follow',authMiddleWare,followUser)
+router.put('/:id/unfollow',authMiddleWare,unfollowUser)
+export default router
